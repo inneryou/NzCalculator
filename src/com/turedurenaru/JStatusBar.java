71ddbada2e7b0b2fb508f6e065440cb98a366a1c
inneryou <inneryou@yahoo.co.jp>
2023-03-04 16:04:35 +0900

    クラスパス設定

--- src/com/turedurenaru/JStatusBar.java
+++ src/com/turedurenaru/JStatusBar.java
@@ -2,15 +2,41 @@ package com.turedurenaru;
 
 import java.awt.*;
 
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.Size;
+import com.jgoodies.forms.layout.*;
 
 public class JStatusBar extends JPanel{
     private JPanel contentPanel;
+    private JLabel resizeIconLabel;
+    private JPanel rightPanel;
+    private FormLayout layout;
+    private int layoutCoordinateX = 1;
+    private int layoutCoordinateY = 1;
+
     public JStatusBar(){
+        setLayout(new BorderLayout());
         setPreferredSize(new Dimension(getWidth(), 23));
+        resizeIconLabel = new JLabel(new TriangleSquareWindowsCornerIcon());
+        resizeIconLabel.setOpaque(false);
+        rightPanel = new JPanel(new BorderLayout());
+
+        rightPanel.setOpaque(false);
+        rightPanel.add(resizeIconLabel, BorderLayout.SOUTH);
+        add(rightPanel, BorderLayout.EAST);
+
+        contentPanel = new JPanel();
+        JLabel testLabel = new JLabel("これはテストです");
+        contentPanel.add(testLabel);
+        contentPanel.setOpaque(false);
+        add(contentPanel, BorderLayout.WEST);
+
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -38,18 +64,20 @@ public class JStatusBar extends JPanel{
         g.setColor(new Color(221,221,220));
         g.drawLine(0,y,getWidth(),y);
 
-        JLabel resizeIconLabel = new JLabel(new TriangleSquareWindowsCornerIcon());
-        resizeIconLabel.setOpaque(false);
+        // layout = new FormLayout("2dlu, pref:grow","3dlu, fill:10dlu, 2dlu");
+        // contentPanel.setLayout(layout);
+    }
 
-        JPanel rightPanel = new JPanel(new BorderLayout());
-        rightPanel.setOpaque(false);
-        rightPanel.add(resizeIconLabel, BorderLayout.SOUTH);
-        add(rightPanel, BorderLayout.EAST);
+    public void setMainLeftComponent(JComponent component){
+        // contentPanel.add(component, new CellConstraints(2,2));
+    }
+    public void setRightComponent(JComponent component, int dialogUnits){
+        // layout.appendColumn(new ColumnSpec("2dlu"));
+        // layout.appendColumn(new ColumnSpec(dialogUnits + "dlu"));
 
-        contentPanel = new JPanel();
-        contentPanel.setOpaque(false);
-        add(contentPanel, BorderLayout.CENTER);
-
-        // FormLayout layout = new FormLayout();
+        layoutCoordinateX++;
+        // contentPanel.add(new SeparatorPanel(Color.GRAY, Color.WHITE),new CellConstraints(layoutCoordinateX, layoutCoordinateY));
+        layoutCoordinateX++;
+        // contentPanel.add(component, new CellConstraints(layoutCoordinateX, layoutCoordinateY));
     }
 }
--- src/com/turedurenaru/MainFrame.java
+++ src/com/turedurenaru/MainFrame.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MainFrame extends JFrame{
     BasePanel basePanel;
@@ -12,6 +14,14 @@ public class MainFrame extends JFrame{
         setTitle(title);
         basePanel = new BasePanel(this);
         JStatusBar statusBar = new JStatusBar();
+        JLabel leftLabel = new JLabel("これはテストです");
+        statusBar.setMainLeftComponent(leftLabel);
+        JLabel dateLabel = new JLabel("2023/3/4");
+        dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        statusBar.setRightComponent(dateLabel, 30);
+        JLabel timeLabel = new JLabel("11:59 PM");
+        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        statusBar.setRightComponent(timeLabel, 30);
         BottomPanel bp = new BottomPanel();
         this.getContentPane().add(basePanel,BorderLayout.CENTER);
         this.getContentPane().add(statusBar,BorderLayout.SOUTH);
